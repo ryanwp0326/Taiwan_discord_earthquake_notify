d3ce7b85bf78218d36d83948cb2a71aa0df531a6
Jude <67894118+judeabc20221@users.noreply.github.com>
2024-04-27 17:34:20 +0800

    package一鍵簡易安裝檔
    只是將所有安裝指令包在一起 不確定是否有bug

---
+++ package_install.bat
@@ -0,0 +1,32 @@
+@echo off
+chcp 65001 > nul
+
+echo 安裝 sys
+pip install sys
+
+echo 安裝 requests
+pip install requests
+
+echo 安裝 time
+pip install time
+
+echo 安裝 os
+pip install os
+
+echo 安裝 io
+pip install io
+
+echo 安裝 discord
+pip install discord
+
+echo 安裝 dotenv
+pip install python-dotenv
+
+echo 安裝 discord_webhook
+pip install discord_webhook
+
+echo 安裝 datetime
+pip install datetime
+
+echo 安裝完成!
+pause
